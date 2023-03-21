4b4a999e94d3716617a72b2ba0d654dfafc9b4ac
Maria Moreno Cifuentes <mmorenocifuentes@gmail.com>
2023-03-21 19:45:47 +0100

    añadido modelos

--- js/test.js
+++ js/test.js
@@ -1,148 +1,288 @@
-/**
-  test.js
-  Ejemplo Three.js_r140: Cubo RGB con iluminacion y textura
+import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js";
+import { DragControls } from "https://cdn.jsdelivr.net/npm/three@0.115/examples/jsm/controls/DragControls.js";
 
-  Cubo con color por vertice y mapa de uvs usando la clase BufferGeometry.
-  La textura es una unica imagen en forma de cubo desplegado en cruz horizontal.
-  Cada cara se textura segun mapa uv en la textura.
-  En sentido antihorario las caras son:
-    Delante:   7,0,3,4
-    Derecha:   0,1,2,3
-    Detras:    1,6,5,2
-    Izquierda: 6,7,4,5
-    Arriba:    3,2,5,4
-    Abajo:     0,7,6,1
-  Donde se han numerado de 0..7 los vertices del cubo.
-  Los atributos deben darse por vertice asi que necesitamos 8x3=24 vertices pues
-  cada vertice tiene 3 atributos de normal, color y uv al ser compartido por 3 caras. 
+var dragControls, orbitControls;
+var scene, renderer, camera;
+var objects = [];
+var dragObject = [];
+var boardObjects = [];
+var raycaster = new THREE.Raycaster();
+var INTERSECTED, intersects;
+var tableHeight = 8.2;
+var gui, board, mouse;
 
-  @author Maria Moreno
-*/
 
-import * as THREE from "../lib/three.module.js";
-import {GLTFLoader} from "../lib/GLTFLoader.module.js";
-import {OrbitControls} from "../lib/OrbitControls.module.js";
-import {TWEEN} from "../lib/tween.module.min.js";
-import {GUI} from "../lib/lil-gui.module.min.js";
+const greyMaterial = new THREE.MeshPhongMaterial({ color: 0x808080 });
+const whiteMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
+const brownMaterial = new THREE.MeshPhongMaterial({ color: 0xa52a2a });
+const floorMaterial = new THREE.MeshBasicMaterial({ color: 0xb5b2b2 });
 
-var renderer, scene, camera, cubo;
-var cameraControls;
+
+//Camera
+// Instanciar la camara
+function createCamera() {
+  camera = new THREE.PerspectiveCamera(
+    90,
+    window.innerWidth / window.innerHeight,
+    1,
+    100
+  );
+  camera.position.z = -5;
+  camera.position.y =50;
+  camera.position.x = -15;
+}
+
+function createScene() {
+  //Scene
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x8e8e8e);
+}
+
+function createRenderer() {
+  //Renderer
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(innerWidth, innerHeight);
+  renderer.shadowMap.enabled = true;
+  document.body.appendChild(renderer.domElement);
+}
+
+//Light
+function createLight() {
+  const ambiental = new THREE.AmbientLight(0x222222);
+  scene.add(ambiental);
+
+  const direccional = new THREE.DirectionalLight(0xffffff, 0.4);
+  direccional.position.set(0, 30, 0);
+  direccional.castShadow = true;
+  scene.add(direccional);
+  // scene.add(new THREE.CameraHelper(direccional.shadow.camera));
+
+  const focal = new THREE.SpotLight(0xffffff, 0.4);
+  focal.position.set(-12, 15, 2);40,40,40,40
+  focal.target.position.set(5, 10, 3);
+  focal.angle = Math.PI / 7;
+  focal.penumbra = 0.3;
+  focal.castShadow = true;
+  focal.shadow.camera.far = 20;
+  focal.shadow.camera.fov = 80;
+  scene.add(focal);
+  scene.add(focal.target);
+  // scene.add(new THREE.CameraHelper(focal.shadow.camera));
+}
+
+//create floor
+function createRoom() {
+  var wallImage = new THREE.MeshBasicMaterial({
+    side: THREE.BackSide,
+    map: new THREE.TextureLoader().load(
+      "./hell.avif"
+    ),
+  });
+
+  var floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(100,100,100,100),
+    wallImage
+  );
+  floor.rotation.x = Math.PI / 2;
+  floor.position.y = -0.2;
+  floor.castShadow = floor.receiveShadow = true;
+  scene.add(floor);
+
+  var roof = new THREE.Mesh(
+    new THREE.PlaneGeometry(100,100,100,100),
+    wallImage
+  );
+  roof.rotation.x = -Math.PI / 2;
+  roof.position.y = 100;
+  roof.castShadow = floor.receiveShadow = true;
+  scene.add(roof);
+
+  const walls = [];
+  
+  walls.push(wallImage);
+  walls.push(wallImage);
+  walls.push(wallImage);
+  walls.push(wallImage);
+
+  const room = new THREE.Mesh( new THREE.BoxGeometry(100,100,100,100), walls)
+  room.rotation.y = Math.PI
+  room.rotation.x = Math.PI/2
+  room.rotation.z = Math.PI/2
+  room.position.y = 50
+  scene.add(room)
+}
+
+function mapTexture(texturePath) {
+  var texture = new THREE.TextureLoader().load(
+    texturePath
+  );
+  const material = new THREE.MeshStandardMaterial({
+    map: texture
+  })
+  return material;
+}
+
+//create chess board
+function createChessBoard() {
+
+  const glloader = new GLTFLoader();
+  glloader.load("models/chess-table/scene.gltf", function (gltf) {
+    gltf.scene.position.set(3.5,7.5,3.5);
+    gltf.scene.scale.set(0.4, 0.5, 0.4)
+    const material = mapTexture("models/chess-board/textures/lambert3SG_baseColor.png")
+    gltf.scene.traverse((o) => {
+      if (o.isObject3D) {
+        o.castShadow = o.receiveShadow = true;
+        o.material = material
+      }
+    });
+    scene.add(gltf.scene);
+    board = gltf.scene;
+  });
+
+}
+
+function loadPiece(object, position, material) {
+  var loader = new GLTFLoader();
+  loader.load(object, function (gltf) {
+    gltf.scene.position.set(position.x, position.y, position.z);
+    gltf.scene.traverse((o) => {
+      if (o.isMesh) {
+        o.material = material;
+      }
+      if (o.isObject3D) {
+        o.castShadow = o.receiveShadow = true;
+      }
+    });
+    scene.add(gltf.scene);
+    objects.push(gltf.scene);
+  });
+}
+
+//Animation
+var animate = function () {
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+};
+
+//create drag controls
+function createDragControls() {
+  dragControls = new DragControls(dragObject, camera, renderer.domElement);
+
+  //hold shift to move object
+  window.addEventListener("keydown", function (event) {
+    console.log(event.key);
+    if (event.key == "Shift") {
+      orbitControls.enabled = false;
+    }
+  });
+
+  window.addEventListener("keyup", function (event) {
+    if (event.key == "Shift") {
+      orbitControls.enabled = true;
+    }
+  });
+}
+
+window.addEventListener("mousedown", raycast, false);
+window.addEventListener("mousemove", onMouseMove, false);
+
+function raycast(event) {
+  dragObject = [];
+
+  let x = event.clientX;
+  let y = event.clientY;
+  x = (x / window.innerWidth) * 2 - 1;
+  y = -(y / window.innerHeight) * 2 + 1;
+
+  raycaster.setFromCamera(new THREE.Vector2(x, y), camera);
+
+  objects.forEach((object) => {
+    intersects = raycaster.intersectObjects(object.children, true);
+    if (intersects.length > 0) {
+      INTERSECTED = intersects[0].object;
+      dragObject.push(INTERSECTED);
+      createDragControls();
+    }
+  });
+}
+
+function loadPawns(material, row) {
+  for (let column = 0; column < 8; column++) {
+    loadPiece(
+      "models/checker/scene.gltf",
+      new THREE.Vector3(column, tableHeight, row),
+      material
+    );
+  }
+}
+
+function onMouseMove(event) {
+  mouse.set((event.clientX / window.innerWidth) * 2 - 1, -(event.clientY / window.innerHeight) * 2 + 1);
+  raycaster.setFromCamera(mouse, camera);
+  intersects = raycaster.intersectObjects([building]);
+
+  if (intersects.length == 0 || !dragging) return;
+
+  normalMatrix.getNormalMatrix(intersects[0].object.matrixWorld);
+  worldNormal.copy(intersects[0].face.normal).applyMatrix3(normalMatrix).normalize();
+  _window.position.copy(intersects[0].point.setY(-0.5)); // -0.5 = bottom of the wall - half height of the window
+  _window.lookAt(lookAtVector.copy(intersects[0].point).add(worldNormal));
+}
+
+
+
+function loadPieces() {
+  loadPawns(greyMaterial, 1);
+  loadPawns(whiteMaterial, 6);
+  loadPawns(greyMaterial, 0);
+  loadPawns(whiteMaterial, 7);
+}
+
+function loadWorld() {
+  createRoom();
+
+  loadPieces();
+  boardObjects = createChessBoard();
+}
+
+function createGUI() {
+  gui = new dat.GUI();
+
+  var controls = {
+    restart: function () {
+      objects.forEach((element) => {
+        console.log(element);
+        scene.remove(element);
+      });
+      loadPieces();
+    },
+    addClock: true,
+  };
+
+  gui.add(controls, "restart");
+
+  var wallpaperOptions = gui.addFolder('Wallpaper Options')
+  
+}
+
+function init() {
+  //basicScene
+  createScene();
+  createRenderer();
+  createLight();
+  createCamera();
+
+  //controls
+  orbitControls = new OrbitControls(camera, renderer.domElement);
+  createDragControls();
+  createGUI();
+
+  loadWorld();
+
+  animate();
+}
 
 init();
-loadScene();
-setupGUI();
-render();
-
-function init()
-{
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.setClearColor( new THREE.Color(0xFFFFFF) );
-  document.getElementById('container').appendChild( renderer.domElement );
-
-  scene = new THREE.Scene();
-
-  var aspectRatio = window.innerWidth / window.innerHeight;
-  camera = new THREE.PerspectiveCamera( 50, aspectRatio , 0.1, 100 );
-  camera.position.set(0.5,2,7);
-  cameraControls = new OrbitControls( camera, renderer.domElement );
-  cameraControls.target.set(0,1,0);
-  camera.lookAt(0,1,0);
-  window.addEventListener('resize', updateAspectRatio );
-}
-
-function loadScene()
-{
-    const material = new THREE.MeshBasicMaterial( { color: 'yellow', wireframe: true } );
-
-    const geoCubo = new THREE.BoxGeometry( 2,2,2 );
-
-    // Objetos dibujables
-    const cubo = new THREE.Mesh( geoCubo, material );
-    cubo.position.x = -1;
-
-    // Suelo
-    const suelo = new THREE.Mesh( new THREE.PlaneGeometry(10,10, 10,10), material );
-    suelo.rotation.x = -Math.PI / 2;
-    suelo.position.y = -0.1;
-    scene.add(suelo);
-/*
-    // Importar un modelo en json
-    const loader = new THREE.ObjectLoader();
-
-    loader.load( 'models/soldado/soldado.json', 
-        function(objeto){
-            cubo.add(objeto);
-            objeto.position.y = 1;
-        }
-    )
-
-    // Importar un modelo en gltf
-    const glloader = new GLTFLoader();
-
-    glloader.load( 'models/robota/scene.gltf', function ( gltf ) {
-        gltf.scene.position.y = 1;
-        gltf.scene.rotation.y = -Math.PI/2;
-        esfera.add( gltf.scene );
-    
-    }, undefined, function ( error ) {
-    
-        console.error( error );
-    
-    } );
-*/
-    // Objeto contenedor
-    esferaCubo = new THREE.Object3D();
-    esferaCubo.position.y = 1.5;
-
-    // Organizacion del grafo
-    scene.add( esferaCubo);
-    esferaCubo.add( cubo );
-    cubo.add( new THREE.AxesHelper(1) );
-    scene.add( new THREE.AxesHelper(3) );
-
-}
-
-function setupGUI()
-{
-	// Definicion de los controles
-	effectController = {
-		mensaje: 'Prueba',
-		giroY: 0.0,
-		separacion: 0,
-		colorsuelo: "rgb(150,150,150)"
-	};
-
-	// Creacion interfaz
-	const gui = new GUI();
-
-	// Construccion del menu
-	const h = gui.addFolder("Control cubo");
-	h.add(effectController, "mensaje").name("Aplicacion");
-	h.add(effectController, "giroY", -180.0, 180.0, 0.025).name("Giro en Y");
-  h.addColor(effectController, "colorsuelo").name("Color alambres");
-
-}
-
-function updateAspectRatio()
-{
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-}
-
-function update()
-{
-
-  // Lectura de controles en GUI (es mejor hacerlo con onChange)
-	cubo.material.setValues( { color: effectController.colorsuelo } );
-  TWEEN.update();
-
-}
-
-function render()
-{
-	requestAnimationFrame( render );
-	update();
-	renderer.render( scene, camera );
-}
